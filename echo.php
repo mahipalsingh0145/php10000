98da0df0fef81f7f135b52c257293009ce330000
Educative Learner <educative_learner@example.com>
2021-09-26 00:29:06 +0530

    Create echo.php

---
+++ echo.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "hi";
+
+echo "<br>";
+
+echo "hi" . "bye";
+echo "<br>";
+
+echo "<b>hi" . "</b>apple";
+
+echo "<br>";
+
+echo "<h><i>hi" . "</h></i>apple";
+
+echo "<br>";
+
+echo 456;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
