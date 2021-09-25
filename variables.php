b9455ce30a56729a09f68c6c6b95e9a43ef94af2
Educative Learner <educative_learner@example.com>
2021-09-26 00:27:19 +0530

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = "hi";
+
+echo $a;
+
+//write way
+//$firstname =
+//$_firstname =
+//$first_name =
+//first-name =
+//$firstName =
+//$firstname99 =
+//not same
+//$age =
+//$Age = 
+
+
+
+?>
